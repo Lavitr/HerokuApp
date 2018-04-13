ce673e582defc2f530c9ad6569975eeca4a0f3c6
Lavitr <lavitr@yandex.ru>
2018-04-13 18:01:32 +0300

    project updated

--- index.js
+++ index.js
@@ -1,106 +1,63 @@
-/*const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-*/
+const app = express();
 
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
-/********************************************/
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const url = process.env.MONGOLAB_URI;
 
-
-//var url = 'mongodb://localhost:27017/node-demo';//local DB
-//var url='mongodb://ron:vitebsk@ds137019.mlab.com:37019/ronlavit' ;
-var url= process.env.MONGOLAB_URI;
-
-var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect(url);
-var nameSchema = new mongoose.Schema({
- title: String,
- articleText:String,
- fullName: String
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  articleText: String,
+  fullName: String
 });
-var Article = mongoose.model("Article", nameSchema);
+const Article = mongoose.model("Article", articleSchema);
 
 
-
-/****************************************************/
-
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-/****************************/
-app.get('/articles',function(req,response){
-  Article.find({},{_id:0,title:1,articleText:1,fullName:1}).limit(5).exec(function (err, articles) {
-   if (err) return console.error(err);
-   console.log(articles);
-
-    response.render('pages/articles',{articles:articles});
- })
-
-})
-/******************************/
-
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.render('pages/index')
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
 
-var pg = require('pg');
-/*
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});*/
+app.get('/articles', function (req, response) {
+  Article.find({}, { _id: 0, title: 1, articleText: 1, fullName: 1 }).limit(5).exec(function (err, articles) {
+    if (err) return console.error(err);
+    response.render('pages/articles', { articles: articles });
+  })
+
+})
 
 app.post("/addname", (req, res) => {
-  Article.find({}).exec(function(err,articles){
+
+  /***check if DB is full(contains five items) remove one item ***/
+  Article.find({}).exec(function (err, articles) {
     if (err) return console.error(err);
-    if(articles.length>=5){
-        Article.findOneAndRemove().exec() ;
+    if (articles.length >= 5) {
+      Article.findOneAndRemove().exec();
     }
   })
- var myData = new Article(req.body);
- myData.save()
- .then(item => {
- res.send("item saved to database");
- Article.find({},{_id:0,title:1,fullName:1}).exec(function (err, articles) {
-  if (err) return console.error(err);
-})
-//Kitten.find({ name: /^Ron/ }, console.log);
- })
- .catch(err => {
- res.status(400).send("unable to save to database");
- });
+  /*** end check ***/
+
+  const myData = new Article(req.body);
+  myData.save()
+    .then(item => {
+      res.send(`<h2>item with title:<span><i>"${req.body.title}"</i></span> saved to database</h2>
+      <a style="font-size:25px" href="/..">Home page</a>`);
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
 });
